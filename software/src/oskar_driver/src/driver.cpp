543052582417f93d3c40b028a5cb62c1bf745b52
Rando Hinn <rando@randohinn.com>
2020-12-19 16:26:57 +0200

    Litsents juurkausta

--- software/src/oskar_driver/src/driver.cpp
+++ software/src/oskar_driver/src/driver.cpp
@@ -3,17 +3,17 @@
 namespace ahhaa_oskar {
 
     Driver::Driver() {
-        ROS_INFO("Initializing OSKAR-III driver");
+        ROS_INFO("Initializing Oskar-III driver");
 
         timer_ = nh_.createTimer(ros::Duration(0.01), &Driver::update, this);
     }
 
     Driver::~Driver() {
-
+        ROS_INFO("Killing Oskar-III driver");
     }
 
     void Driver::update(const ros::TimerEvent& event) {
-        ROS_INFO("OSKAR III Update");
+        ROS_INFO("Oskar III Update");
 
     }
 
